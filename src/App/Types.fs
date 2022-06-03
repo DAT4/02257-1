a5e9a152f0481ccde8d40d3cdc80e0c7bd610720
Johan Ott <johanrott@gmail.com>
2022-06-03 11:00:32 +0200

    Update on Types.fs

--- src/App/Types.fs
+++ src/App/Types.fs
@@ -8,13 +8,13 @@ type Extend = (float*float) list;;
 
 let moveextend (e : Extend, x) : Extend = List.map (fun (p,q) -> (p+x,q+x)) e;;
 
-let rec merge (ps, qs)  =
+let rec merge ps qs  =
     match (ps,qs) with
     | ([], qs) -> qs
     | (ps, []) -> ps
-    | ((p,_)::ps', (_,q)::qs') -> (p,q)::merge (ps', qs')
+    | ((p,_)::ps', (_,q)::qs') -> (p,q)::(merge ps' qs')
 
-let mergelist es = List.fold merge es []
+let mergelist es = List.fold merge [] es
 
 let rmax (p:float, q:float) = if p > q then p else q
 
@@ -26,7 +26,7 @@ let fitlistl es =
         | [] -> []
         | (e::es'') -> 
             let x = fit acc e
-            x::fitlistl' (merge (acc, moveextend (e,x))) es''
+            x::fitlistl' (merge acc (moveextend (e,x))) es''
     fitlistl' [] es
 
 let fitlistr es =
@@ -35,9 +35,23 @@ let fitlistr es =
         | [] -> []
         | (e::es'') -> 
             let x = fit acc e
-            x::fitlistr' (merge (moveextend (e,x), acc)) es''
+            x::fitlistr' (merge (moveextend (e,x)) acc) es''
     fitlistr' [] es
 
 let mean (x, y) = (x+y)/2.0
 
-let fitlist es = List.map mean (List.zip (fitlistl es, fitlistr es))
+let fitlist es = List.map mean (List.zip (fitlistl es) (fitlistr es))
+
+
+
+let design tree =
+    let rec design' (Node(label, subtrees)) =
+        let (trees, extends)    = List.unzip (List.map design' subtrees)
+        let positions           = fitlist extends
+        let ptrees              = List.map (movetree (List.zip trees positions))
+        let ptextends           = List.map moveextend (List.zip extends positions)
+        let resultextend       = (0.0, 0.0) :: mergelist ptextends
+        let resulttree          = Node((label, 0.0), ptrees)
+        (resulttree, resultextend)
+    fst (design' tree)
+
