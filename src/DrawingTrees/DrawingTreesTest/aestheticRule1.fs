973d82bfc0b91dfa7795b7f025d093f03e650061
Johan Ott <johanrott@gmail.com>
2022-06-08 11:58:52 +0200

    working on the aesthetic rule 2

--- src/DrawingTrees/DrawingTreesTest/aestheticRule1.fs
+++ src/DrawingTrees/DrawingTreesTest/aestheticRule1.fs
@@ -1,1 +1,2 @@
+/// Aesthethic 1: Two nodes at the same level should be placed at least a given distance apart.
 module AestheticRule1
--- src/DrawingTrees/DrawingTreesTest/aestheticRule4.fs
+++ src/DrawingTrees/DrawingTreesTest/aestheticRule4.fs
@@ -1,1 +1,4 @@
+/// Aesthethic 3:  Identical subtrees should be rendered identically—their position in the larger 
+/// tree should not affect their appearance. In Figure 2 the tree on the left fails 
+/// the test, and the one on the right passes.
 module AestheticRule4
