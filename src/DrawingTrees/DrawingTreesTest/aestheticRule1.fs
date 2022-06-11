f55d0e4bb4a34e31b6e49b8d4b0d9f26270ccb00
Martin Maartensson <mail@mama.sh>
2022-06-11 23:08:22 +0200

    remove import

--- src/DrawingTrees/DrawingTreesTest/aestheticRule1.fs
+++ src/DrawingTrees/DrawingTreesTest/aestheticRule1.fs
@@ -6,7 +6,6 @@
 // Aesthethic 1: Two nodes at the same level should be placed at least a given distance apart.
 module AestheticRule1
 
-open FsCheck
 open FsCheck.NUnit
 open TreeTypes
 open TestUtils
