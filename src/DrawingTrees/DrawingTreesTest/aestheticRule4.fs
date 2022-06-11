3f00f1742437e3e19b7c948e2b120171dc94fbbe
Johan Ott <johanrott@gmail.com>
2022-06-11 04:05:43 +0200

    Another take on rule 4

--- src/DrawingTrees/DrawingTreesTest/aestheticRule4.fs
+++ src/DrawingTrees/DrawingTreesTest/aestheticRule4.fs
@@ -41,16 +41,34 @@ open NUnit.Framework
 let equalTreesAreIdentical () =
     Assert.IsTrue(testProperty equalTreesAreIdenticalProperty Program.t)
 
-(*
-let rec relativeDistanceToParent acc (PosNode(_, x, subtrees)) =
-    match subtrees with
-    | [] -> acc
-    | sts -
+let compareRelativeDistance x1 x2 (PosNode(_, subtreex1, _)) (PosNode(_, subtreex2, _)) =
+    x1-subtreex1 = x2-subtreex2
+
+let rec compareTreeShapes (tree1 : PosTree<'a>, tree2 : PosTree<'a>) =
+    match  (tree1, tree2) with
+    | (PosNode(_, _, subtrees1), PosNode(_, _, subtrees2))
+        when List.length subtrees1 <> List.length subtrees2 -> false
+    | (PosNode(_, x1, subtrees1), PosNode(_, x2, subtrees2)) ->
+        List.forall2 (compareRelativeDistance x1 x2) subtrees1 subtrees2
+            && List.zip subtrees1 subtrees2  |> List.forall compareTreeShapes
 
 let movingTreeIsUnalteredProperty x tree =
-    moveTree x tree = tree
+    compareTreeShapes ((moveTree x tree), tree)
 
 [<Test>]
 let movingTreeIsUnaltered () =
     Assert.IsTrue(movingTreeIsUnalteredProperty 1.0 (designTree Program.t))
-*)
+
+let rec comparExtentShapes (extent1 : Extent, extent2 : Extent) =
+    match (extent1, extent2) with
+    | (extent1, extent2)
+        when (List.length extent1) <> (List.length extent2) -> false
+    | ((l1,r1)::extent1rest, (l2,r2)::extent2rest) -> 
+        l1-r1 = l2-r2 && comparExtentShapes (extent1rest, extent2rest)
+
+let movingExtentIsUnalteredProperty x extent =
+    comparExtentShapes (moveExtent x extent, extent)
+
+[<Test>]
+let movingExtentIsUnaltered () =
+    Assert.IsTrue(movingExtentIsUnalteredProperty 1.0 (designExtents Program.t))
