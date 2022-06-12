20af04bc0c1a6ab99463826cf059966d007ecfb0
Johan Ott <johanrott@gmail.com>
2022-06-12 21:35:54 +0200

    minor code cleanup

--- src/DrawingTrees/DrawingTreesTest/aestheticRule4.fs
+++ src/DrawingTrees/DrawingTreesTest/aestheticRule4.fs
@@ -2,6 +2,9 @@
 // ./src/DrawingTrees/DrawingTreesTest/aestheticRule4.fs
 // --------------------------------------------------
 // 
+// Author: Johan Ott 
+// Date:   Sun Jun 12 21:35:43 
+// 
 // Author: Martin Maartensson 
 // Date:   Sat Jun 11 11:50:52 
 // 
@@ -30,66 +33,36 @@ module AestheticRule4
 open TreeTypes
 open PositionedTree
 open TestUtils
+open NUnit.Framework
 
-let rec getListOfListOfPositions acc (PosNode(_, x, subtrees)) =
-        match subtrees with
-        | [] -> []::acc
-        | _ ->
-            let test = subtrees |> List.map getSubtreePositions
-            let d = subtrees |> List.map (getListOfListOfPositions []) |> List.concat
-            test::d
+let compareRelativeDistance x1 x2 (PosNode(_, subtreex1, _)) (PosNode(_, subtreex2, _)) =
+    x1-subtreex1 = x2-subtreex2
 
-let areListsIdentical (numberOfSubtrees : int, listOfListOfPositions : float list list) =
-    (listOfListOfPositions |> List.distinct |> List.length) = 1
+let rec compareTreeShapes (tree1 : PosTree<'a>, tree2 : PosTree<'a>) =
+    match  (tree1, tree2) with
+    | (PosNode(_, _, subtrees1), PosNode(_, _, subtrees2))
+        when List.length subtrees1 <> List.length subtrees2 -> false
+    | (PosNode(_, x1, subtrees1), PosNode(_, x2, subtrees2)) ->
+        List.forall2 (compareRelativeDistance x1 x2) subtrees1 subtrees2
+            && List.zip subtrees1 subtrees2  |> List.forall compareTreeShapes
 
-let equalTreesAreIdenticalProperty tree =
-    let listOfListOfPositions = getListOfListOfPositions [] tree
-    let test =
-        listOfListOfPositions |> List.groupBy List.length
-                              |> List.forall areListsIdentical
+let movingTreeIsUnalteredProperty x tree =
+    compareTreeShapes ((moveTree x tree), tree)
 
+[<Test>]
+let movingTreeIsUnaltered () =
+    Assert.IsTrue(movingTreeIsUnalteredProperty 1.0 (designTree Program.t))
 
-    test
-(*
-getListOfListOfPositions [] (designTree idSubTree)
-getListOfListOfPositions [] (designTree idSubTree)|> List.groupBy List.length
-getListOfListOfPositions [] (designTree idSubTree)|> List.groupBy List.length |> List.forall areListsIdentical
-*)
+let rec comparExtentShapes (extent1 : Extent, extent2 : Extent) =
+    match (extent1, extent2) with
+    | (extent1, extent2)
+        when (List.length extent1) <> (List.length extent2) -> false
+    | ((l1,r1)::extent1rest, (l2,r2)::extent2rest) -> 
+        l1-r1 = l2-r2 && comparExtentShapes (extent1rest, extent2rest)
 
-//[<Property>]
-//open NUnit.Framework
-//[<Test>]
-//let equalTreesAreIdentical () =
-//    Assert.IsTrue(testProperty equalTreesAreIdenticalProperty Program.t)
-//
-//let compareRelativeDistance x1 x2 (PosNode(_, subtreex1, _)) (PosNode(_, subtreex2, _)) =
-//    x1-subtreex1 = x2-subtreex2
-//
-//let rec compareTreeShapes (tree1 : PosTree<'a>, tree2 : PosTree<'a>) =
-//    match  (tree1, tree2) with
-//    | (PosNode(_, _, subtrees1), PosNode(_, _, subtrees2))
-//        when List.length subtrees1 <> List.length subtrees2 -> false
-//    | (PosNode(_, x1, subtrees1), PosNode(_, x2, subtrees2)) ->
-//        List.forall2 (compareRelativeDistance x1 x2) subtrees1 subtrees2
-//            && List.zip subtrees1 subtrees2  |> List.forall compareTreeShapes
-//
-//let movingTreeIsUnalteredProperty x tree =
-//    compareTreeShapes ((moveTree x tree), tree)
-//
-//[<Test>]
-//let movingTreeIsUnaltered () =
-//    Assert.IsTrue(movingTreeIsUnalteredProperty 1.0 (designTree Program.t))
-//
-//let rec comparExtentShapes (extent1 : Extent, extent2 : Extent) =
-//    match (extent1, extent2) with
-//    | (extent1, extent2)
-//        when (List.length extent1) <> (List.length extent2) -> false
-//    | ((l1,r1)::extent1rest, (l2,r2)::extent2rest) -> 
-//        l1-r1 = l2-r2 && comparExtentShapes (extent1rest, extent2rest)
-//
-//let movingExtentIsUnalteredProperty x extent =
-//    comparExtentShapes (moveExtent x extent, extent)
-//
-//[<Test>]
-//let movingExtentIsUnaltered () =
-//    Assert.IsTrue(movingExtentIsUnalteredProperty 1.0 (designExtents Program.t))
+let movingExtentIsUnalteredProperty x extent =
+    comparExtentShapes (moveExtent x extent, extent)
+
+[<Test>]
+let movingExtentIsUnaltered () =
+    Assert.IsTrue(movingExtentIsUnalteredProperty 1.0 (designExtents Program.t))
